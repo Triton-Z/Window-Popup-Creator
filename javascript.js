b23bd456cf5cc6c8aa2284e722a16f9767cb09bd
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-11-28 21:26:34 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
 document.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        window.open(document.getElementById("txt").value,'window','width=600,height=300');
+    const input = document.getElementById("txt");
+    if (event.keyCode === 13) 
+    {
+        if (input.value.substring(0, 7) === "https://" || input.value.substring(0, 6) === "http://") 
+        {
+            window.open(input.value,'window','width=600,height=300');
+        } else
+        {
+            window.open("https://" + input.value,'window','width=600,height=300');   
+        }
     }
 });
